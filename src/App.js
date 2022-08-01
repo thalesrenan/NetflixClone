a566403cc7de6b75661a574ccf886022c2572b1e
Thales Renan <thalesrmn@hotmail.com>
2022-08-01 16:53:00 -0300

    FIX: user image is now appearing again.

--- src/App.js
+++ src/App.js
@@ -1,75 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Tmdb from './Tmdb';
-import MovieRow from './components/MovieRow';
-import FeaturedMovie from './components/FeaturedMovie'
-import Header from './components/Header'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 export default () => {
-
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
   const [blackHeader, setBlackHeader] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     const loadAll = async () => {
       //Pegando lista
       let list = await Tmdb.getHomeList();
       setMovieList(list);
 
       //Filme em destaque
-      let originals = list.filter(i => i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
       let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
       setFeaturedData(chosenInfo);
-    }
+    };
 
     loadAll();
   }, []);
 
   useEffect(() => {
     const scrollListener = () => {
-      if(window.scrollY > 10) {
+      if (window.scrollY > 10) {
         setBlackHeader(true);
       } else {
         setBlackHeader(false);
       }
-    }
+    };
 
-    window.addEventListener('scroll', scrollListener);
+    window.addEventListener("scroll", scrollListener);
 
     return () => {
-      window.removeEventListener('scroll', scrollListener)
-    }
+      window.removeEventListener("scroll", scrollListener);
+    };
   }, []);
-  
-  return(
-    <div className="page">
 
+  return (
+    <div className="page">
       <Header black={blackHeader} />
 
-      {featuredData &&
-        <FeaturedMovie item={featuredData} />
-      }
+      {featuredData && <FeaturedMovie item={featuredData} />}
 
       <section className="lists">
-        {movieList.map((item, key)=>(
+        {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
 
       <footer>
-        Feito por Thales Renan<br/>
-        Direitos de imagem para Netflix<br/>
-        Dados pegos do site Themoviedb.org<br />
+        Feito por Thales Renan
+        <br />
+        Direitos de imagem para Netflix
+        <br />
+        Dados pegos do site Themoviedb.org
+        <br />
       </footer>
 
-      {movieList.length <= 0 &&
+      {movieList.length <= 0 && (
         <div className="loading">
-          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+          <img
+            src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+            alt="Carregando"
+          />
         </div>
-      }
+      )}
     </div>
   );
-}
+};
